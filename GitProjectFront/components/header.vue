be753a734b495a14c38480e5e767d0c7c926c672
Jérémie Fournier <jeremie.fournier@hotmail.com>
2023-09-27 16:44:51 +0200

    header

--- GitProjectFront/components/header.vue
+++ GitProjectFront/components/header.vue
@@ -2,17 +2,17 @@
     <header class="bg-blue-500 py-4">
       <div class="container mx-auto">
         <nav class="flex items-center justify-between">
-          <div class="text-white font-bold text-xl ml-10 flex items-center">
+          <div class="text-white font-bold text-3xl ml-10 flex items-center">
             <p>
                 Blague de
             </p>
-            <img class="h-11"  src="../assets/img/logo.png" alt="">
+            <img class="h-24"  src="../assets/img/logo.png" alt="">
           </div>
-          <ul class="flex space-x-4 mr-10 gap-10">
-            <li><a href="#" class="text-white hover:text-gray-300">Accueil</a></li>
-            <li><a href="#" class="text-white hover:text-gray-300">À propos</a></li>
-            <li><a href="#" class="text-white hover:text-gray-300">Services</a></li>
-            <li><a href="#" class="text-white hover:text-gray-300">Contact</a></li>
+          <ul class="flex space-x-4 mr-24 gap-16">
+            <NuxtLink to="/" class="text-white font-bold text-xl hover:text-gray-300">Accueil</NuxtLink>
+            <NuxtLink to="/blagues" class="text-white font-bold text-xl hover:text-gray-300">Blagues</NuxtLink>
+            <NuxtLink to="/a-propos" class="text-white font-bold text-xl hover:text-gray-300">À Propos</NuxtLink>
+            
           </ul>
         </nav>
       </div>
