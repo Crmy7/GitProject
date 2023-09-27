d08668cabbb6ada3208086131c3cb3f5ce5f37ab
Crmy7 <charlesremy03@gmail.com>
2023-09-27 15:54:25 +0200

    Feature api {id}  single joke

--- GitProjectBack/src/Controller/JokeController.php
+++ GitProjectBack/src/Controller/JokeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Joke;
+use App\Entity\jokes;
 use App\Repository\JokeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -28,6 +30,28 @@ class JokeController extends AbstractController
 
         // Créez une réponse JSON
         $response = new JsonResponse($jokesData);
+
+        // Ajoutez l'en-tête "Access-Control-Allow-Origin" pour autoriser les requêtes CORS depuis n'importe quelle origine
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        // Retournez la réponse
+        return $response;
+    }
+
+    #[Route('/api/jokes/{id}', name: 'api_joke_show')]
+    public function apiJokeShow(Joke $joke): JsonResponse
+    {
+        // Convertissez le produit en tableau associatif
+        $jokeData = [
+            'id' => $joke->getId(),
+            'title' => $joke->getTitle(),
+            'content' => $joke->getContent(),
+            'creationDate' => $joke->getCreationDate(),
+            'author' => $joke->getAuthor()
+        ];
+
+        // Créez une réponse JSON
+        $response = new JsonResponse($jokeData);
 
         // Ajoutez l'en-tête "Access-Control-Allow-Origin" pour autoriser les requêtes CORS depuis n'importe quelle origine
         $response->headers->set('Access-Control-Allow-Origin', '*');
